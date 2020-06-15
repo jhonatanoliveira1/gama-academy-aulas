abdd1b4cf661b1424fde23423eef13a48f797c3a
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2020-06-15 12:14:03 -0300

    buscador de repositórios pelo usuario do github

---
+++ gama-aula-05-react/src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import Routes from './routes';
+
+import './style.css';
+
+function App() {
+
+  return (
+      <Routes />
+
+  );
+}
+
+export default App;
