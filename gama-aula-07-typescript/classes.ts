e93c17ddeca90e097f5866c251054ea678e97a00
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2020-06-28 18:51:32 -0300

    Utilizando Modificadores de Acesso

--- gama-aula-07-typescript/classes.ts
+++ gama-aula-07-typescript/classes.ts
@@ -14,3 +14,29 @@ const data = new Data(1, 1, 2020);
 console.log(data.dia);
 
 const data2 = new Data(1, 1);
+
+class Carro {
+  private velocidadeAtual: number = 0;
+
+  constructor(
+    public marca: string,
+    public modelo: string,
+    private velocidadeMaxima: number = 120
+  ) {}
+
+  private alterarVelocidade(delta: number) {
+    // Faço as validiações de acelaração e freagem
+    // this.velocidadeAtual = xxx
+  }
+
+  acelarar() {
+    this.alterarVelocidade(5);
+  }
+
+  frear() {
+    this.alterarVelocidade(-5);
+  }
+}
+
+const carro = new Carro('Chevrolet', 'Prisma', 250);
+carro.acelarar();
