ef4d6cb8f0c3489902f29b539c5871bfd35bf0a3
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2020-07-11 09:06:07 -0300

    Criando o servidor

--- gama-aula-08-graphQL/src/main.js
+++ gama-aula-08-graphQL/src/main.js
@@ -1,1 +1,29 @@
-console.log('hello');
+import { createServer } from 'http';
+
+const server = createServer((request, response) => {
+  switch (request.url) {
+    case '/status': {
+      response.writeHead(200, {
+        'Content-Type': 'application/json',
+      });
+      response.write(
+        JSON.stringify({
+          status: 'Okay',
+        })
+      );
+      response.end();
+      return;
+    }
+    default: {
+      response.writeHead(404, 'Server not found.');
+      response.end();
+    }
+  }
+});
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8080;
+const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
+
+server.listen(PORT, '127.0.0.1', () => {
+  console.log(`Server is listening at http://${HOSTNAME}:${PORT}.`);
+})
