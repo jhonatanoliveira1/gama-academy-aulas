61305e43169d69f160b89d1b9506ff6676ddde74
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2020-07-12 16:26:59 -0300

    Apollo Server

--- gama-aula-08-graphQL/packeges/server/src/main.js
+++ gama-aula-08-graphQL/packeges/server/src/main.js
@@ -1,27 +1,59 @@
 import express from 'express';
-import cors from 'cors';
+import { ApolloServer, gql } from 'apollo-server-express';
 
-const server = express();
+const app = express();
 
-server.get('/status', (_, response) => {
-  response.send({
-    status: 'Okay',
-  });
+const server = new ApolloServer({
+  typeDefs: gql`
+    type Client {
+      id: ID!
+      name: String!
+    }
+
+    type Demand {
+      id: ID!
+      name: String!
+      client: Client!
+      deadline: String
+    }
+
+    type Query {
+      demands: [Demand]!
+    }
+  `,
+  resolvers: {
+    Query: {
+      demands: () => [],
+    },
+  },
 });
 
-const enableCors = cors({ origin: 'http://localhost:3000' });
-
-server
-  .options('/authenticate', enableCors)
-  .post('/authenticate', enableCors, express.json(), (request, response) => {
-  console.log(
-    'E-mail', request.body.email,
-    'Senha', request.body.password
-  );
-  response.send({
-    Okay: true,
-  })
+server.applyMiddleware({
+  app,
+  cors: {
+    origin: 'http://localhost:3000', 
+  }
 });
+
+// server.get('/status', (_, response) => {
+//   response.send({
+//     status: 'Okay',
+//   });
+// });
+
+// const enableCors = cors({ origin: 'http://localhost:3000' });
+
+// server
+//   .options('/authenticate', enableCors)
+//   .post('/authenticate', enableCors, express.json(), (request, response) => {
+//   console.log(
+//     'E-mail', request.body.email,
+//     'Senha', request.body.password
+//   );
+//   response.send({
+//     Okay: true,
+//   })
+// });
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8080;
 const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
