a3253e15541136ee5d02602c33f190cc7328020f
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2020-07-12 19:01:23 -0300

    Criando GraphQL Queries

--- gama-aula-08-graphQL/packeges/server/src/graphql/Demand/Demand.js
+++ gama-aula-08-graphQL/packeges/server/src/graphql/Demand/Demand.js
@@ -15,6 +15,9 @@ export const typeDefs = gql`
 
 export const resolvers = {
   Query: {
-    demands: () => [],
+    demands: async () => {
+      await new Promise((resolve) => setTimeout(resolve, Math.random() * 2000));
+      return [];
+    },
   },
 };
