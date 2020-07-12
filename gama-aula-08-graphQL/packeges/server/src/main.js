e8ae0e1a2816840786c6feb1164224f0c0343fd4
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2020-07-12 18:27:50 -0300

    GraphQL - typeDefs e resolvers

--- gama-aula-08-graphQL/packeges/server/src/main.js
+++ gama-aula-08-graphQL/packeges/server/src/main.js
@@ -1,63 +1,27 @@
 import express from 'express';
-import { ApolloServer, gql } from 'apollo-server-express';
+
+import { ApolloServer } from 'apollo-server-express';
+import typeDefs from './graphql/typeDefs';
+import resolvers from './graphql/resolvers';
 
 const app = express();
 
 const server = new ApolloServer({
-  typeDefs: gql`
-    type Client {
-      id: ID!
-      name: String!
-    }
-
-    type Demand {
-      id: ID!
-      name: String!
-      client: Client!
-      deadline: String
-    }
-
-    type Query {
-      demands: [Demand]!
-    }
-  `,
-  resolvers: {
-    Query: {
-      demands: () => [],
-    },
-  },
+  typeDefs,
+  resolvers,
 });
 
 server.applyMiddleware({
   app,
   cors: {
-    origin: 'http://localhost:3000', 
-  }
+    origin: 'http://localhost:3000',
+  },
+  bodyParserConfig: true,
 });
 
-// server.get('/status', (_, response) => {
-//   response.send({
-//     status: 'Okay',
-//   });
-// });
-
-// const enableCors = cors({ origin: 'http://localhost:3000' });
-
-// server
-//   .options('/authenticate', enableCors)
-//   .post('/authenticate', enableCors, express.json(), (request, response) => {
-//   console.log(
-//     'E-mail', request.body.email,
-//     'Senha', request.body.password
-//   );
-//   response.send({
-//     Okay: true,
-//   })
-// });
-
-const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8080;
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
 const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
 
-server.listen(PORT, '127.0.0.1', () => {
+app.listen(PORT, HOSTNAME, () => {
   console.log(`Server is listening at http://${HOSTNAME}:${PORT}.`);
-})
+});
