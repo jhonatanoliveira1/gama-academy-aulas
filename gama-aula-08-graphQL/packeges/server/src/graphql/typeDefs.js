5b229caf68e140ed5d414319d7c7e7332badefc7
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2020-07-12 21:57:27 -0300

    paginação, ordenação e filtros

--- gama-aula-08-graphQL/packeges/server/src/graphql/typeDefs.js
+++ gama-aula-08-graphQL/packeges/server/src/graphql/typeDefs.js
@@ -1,5 +1,7 @@
 import { gql } from 'apollo-server-express';
 
+import { typeDefs as nodeTypeDefs } from './Node/Node';
+import { typeDefs as listTypeDefs } from './List/List';
 import { typeDefs as clientTypeDefs } from './Client/Client';
 import { typeDefs as demandTypeDefs } from './Demand/Demand';
 
@@ -7,7 +9,8 @@ const typeDefs = gql`
   type Query {
     _root: String
   }
-
+  ${nodeTypeDefs}
+  ${listTypeDefs}
   ${clientTypeDefs}
   ${demandTypeDefs}
 `;
